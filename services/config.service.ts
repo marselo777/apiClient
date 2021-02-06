f7b2b97e08378330b2adfe5d336df385c3cbabd5
Evgeny Frolov <65085710+slenter777@users.noreply.github.com>
2021-02-06 13:23:42 +0300

    Delete services directory

--- services/config.service.ts
+++
@@ -1,8 +0,0 @@
-class ConfigService {
-  getStaticFileUrl(file: string) {
-    const url = !file.startsWith('http') ? `http://localhost:3010/api/storage/${file}` : file;
-    return url;
-  }
-}
-
-export const configService = new ConfigService();
